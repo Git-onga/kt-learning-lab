14ac1a8e7d423674291688725afb37b5ed2968c7
Bildad <bildadgitoga53@gmail.com>
2024-10-15 13:52:08 +0300

    start-ups files

---
+++ raylib/tetris/main.cpp
@@ -0,0 +1,16 @@
+#include <raylib.h>
+
+int main() {
+
+    InitWindow(300, 600, "raylib tetris");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false ){
+        BeginDrawing();
+
+        EndDrawing();
+    }
+     
+    CloseWindow(); 
+}
+
